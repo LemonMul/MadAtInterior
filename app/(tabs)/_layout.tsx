6547d44e4a52aa2ce07a03823f8532f135ad16de
221B0825 <eunseoyu0825@gmail.com>
2024-04-10 06:47:42 +0900

    feat: customize Tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,39 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { FontAwesome, Ionicons, MaterialIcons } from '@expo/vector-icons'
+import Colors from '@/constants/Colors'
 
 
 export default function _layout() {
   return (
-    <Tabs>
-        <Tabs.Screen name='index' />
-        <Tabs.Screen name='category' />
-        <Tabs.Screen name='search' />
-        <Tabs.Screen name='bookmarks' />
-        <Tabs.Screen name='profile' />
+    <Tabs screenOptions={{
+        tabBarStyle: {
+            backgroundColor: Colors.bgColor,
+            borderTopWidth: 0,
+            padding: 0
+        },
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: Colors.black,
+        tabBarInactiveTintColor: Colors.gray,
+    }}>
+        <Tabs.Screen name='index' options={{tabBarIcon: ({color}) =>(
+            <Ionicons name='compass' size={28} color={color} />
+        ),}} />
+        <Tabs.Screen name='category' options={{tabBarIcon: ({color}) =>(
+            <MaterialIcons name='space-dashboard' size={28} color={color} />
+        ),}} />
+        <Tabs.Screen name='home' options={{tabBarIcon: ({color}) =>(
+            <View style={{backgroundColor:Colors.primaryColor, padding: 10, borderRadius: 10, height: 50}}>
+            <Ionicons name='home' size={28} color={Colors.white} />
+            </View>
+        )}} />
+        <Tabs.Screen name='bookmarks' options={{tabBarIcon: ({color}) =>(
+            <Ionicons name='bookmark' size={28} color={color} />
+        )}} />
+        <Tabs.Screen name='profile' options={{tabBarIcon: ({color}) =>(
+            <FontAwesome name='user' size={28} color={color} />
+        )}} />
     </Tabs>
   )
 }
---
+++ constants/Colors.ts
@@ -0,0 +1,7 @@
+export default {
+    primaryColor: '#4B7541',
+    bgColor: '#F4F4F4',
+    black: '#27283a',
+    white: '#FFFFFF',
+    gray: '#999',
+}
