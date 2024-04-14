6f3ba817ff6bb3958a22dfecf942c722e7a27d94
221B0825 <eunseoyu0825@gmail.com>
2024-04-14 20:38:42 +0900

    feat: add company detail

--- components/CompanyListings.tsx
+++ components/CompanyListings.tsx
@@ -9,13 +9,21 @@ import {
 import React from 'react';
 import { GroupType } from '@/types/CompanyType';
 import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
 
 const GroupListings = ({ listings }: { listings: GroupType[] }) => {
   const renderItem: ListRenderItem<GroupType> = ({ item }) => {
     return (
       <View style={styles.item}>
         <Image source={{ uri: item.image }} style={styles.image} />
-        <View></View>
+        <View>
+          <Text style={styles.itemTxt}>{item.name}</Text>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Ionicons name="star" size={20} color={Colors.primaryColor} />
+            <Text style={styles.itemRating}>{item.rating}</Text>
+            <Text style={styles.itemReview}>({item.reviews})</Text>
+          </View>
+        </View>
       </View>
     );
   };
@@ -39,17 +47,36 @@ const styles = StyleSheet.create({
     width: 80,
     height: 100,
     borderRadius: 10,
+    marginRight: 10,
   },
   item: {
     backgroundColor: Colors.white,
     padding: 10,
     borderRadius: 10,
     marginRight: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   title: {
     fontSize: 22,
     fontWeight: '600',
     color: Colors.black,
     margin: 10,
+  },
+  itemTxt: {
+    fontSize: 14,
+    fontWeight: '600',
+    color: Colors.black,
+    marginBottom: 8,
+  },
+  itemRating: {
+    fontSize: 14,
+    fontWeight: '600',
+    color: Colors.black,
+    marginLeft: 5,
+  },
+  itemReview: {
+    fontSize: 14,
+    color: Colors.gray,
   },
 });
