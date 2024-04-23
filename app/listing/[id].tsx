17d203e2570eda27c18d5d030678e08bd050b582
221B0825 <eunseoyu0825@gmail.com>
2024-04-23 17:18:24 +0900

    feat: add detail page animation

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,6 +1,7 @@
 import {
   Dimensions,
   Image,
+  ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -10,8 +11,20 @@ import React from 'react';
 import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
 import listingData from '@/data/case.json';
 import { ListingType } from '@/types/listingType';
-import { Feather, FontAwesome5, Ionicons } from '@expo/vector-icons';
+import {
+  Feather,
+  FontAwesome,
+  FontAwesome5,
+  Ionicons,
+} from '@expo/vector-icons';
 import Colors from '@/constants/Colors';
+import Animated, {
+  SlideInDown,
+  interpolate,
+  useAnimatedRef,
+  useAnimatedStyle,
+  useScrollViewOffset,
+} from 'react-native-reanimated';
 
 const { width } = Dimensions.get('window');
 const IMG_HEIGHT = 300;
@@ -23,6 +36,30 @@ const ListingDetails = () => {
   );
 
   const router = useRouter();
+
+  const scrollRef = useAnimatedRef<Animated.ScrollView>();
+  const scrollOffset = useScrollViewOffset(scrollRef);
+  const imageAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: interpolate(
+            scrollOffset.value,
+            [-IMG_HEIGHT, 0, IMG_HEIGHT],
+            [-IMG_HEIGHT / 2, 0, IMG_HEIGHT * 0.75]
+          ),
+        },
+        {
+          scale: interpolate(
+            scrollOffset.value,
+            [-IMG_HEIGHT, 0, IMG_HEIGHT],
+            [2, 1, 1]
+          ),
+        },
+      ],
+    };
+  });
+
   if (listing) {
     return (
       <>
@@ -73,19 +110,82 @@ const ListingDetails = () => {
           }}
         />
         <View style={styles.container}>
-          <Image source={{ uri: listing.image }} style={styles.image} />
-          <View style={styles.contentWrapper}>
-            <Text style={styles.listingName}>{listing.name}</Text>
-            <View style={styles.listingLocationWrapper}>
-              <FontAwesome5
-                name="map-marker-alt"
-                size={18}
-                color={Colors.primaryColor}
-              />
-              <Text style={styles.listingLocationTxt}>{listing.location}</Text>
+          <Animated.ScrollView
+            ref={scrollRef}
+            contentContainerStyle={{ paddingBottom: 400 }}
+          >
+            <Animated.Image
+              source={{ uri: listing.image }}
+              style={[styles.image, imageAnimatedStyle]}
+            />
+            <View style={styles.contentWrapper}>
+              <Text style={styles.listingName}>{listing.name}</Text>
+              <View style={styles.listingLocationWrapper}>
+                <FontAwesome5
+                  name="map-marker-alt"
+                  size={18}
+                  color={Colors.primaryColor}
+                />
+                <Text style={styles.listingLocationTxt}>
+                  {listing.location}
+                </Text>
+              </View>
+              <View style={styles.highlightWrapper}>
+                <View style={{ flexDirection: 'row' }}>
+                  <View style={styles.highlightIcon}>
+                    <Ionicons
+                      name="star"
+                      size={18}
+                      color={Colors.primaryColor}
+                    />
+                  </View>
+                  <View>
+                    <Text style={styles.highlightTxt}>Rating</Text>
+                    <Text style={styles.highlightTxtVal}>{listing.rating}</Text>
+                  </View>
+                </View>
+                <View style={{ flexDirection: 'row' }}>
+                  <View style={styles.highlightIcon}>
+                    <Ionicons
+                      name="time"
+                      size={18}
+                      color={Colors.primaryColor}
+                    />
+                  </View>
+                  <View>
+                    <Text style={styles.highlightTxt}>Rating</Text>
+                    <Text style={styles.highlightTxtVal}>{listing.rating}</Text>
+                  </View>
+                </View>
+                <View style={{ flexDirection: 'row' }}>
+                  <View style={styles.highlightIcon}>
+                    <FontAwesome
+                      name="users"
+                      size={18}
+                      color={Colors.primaryColor}
+                    />
+                  </View>
+                  <View>
+                    <Text style={styles.highlightTxt}>Rating</Text>
+                    <Text style={styles.highlightTxtVal}>{listing.rating}</Text>
+                  </View>
+                </View>
+              </View>
+              <Text style={styles.listingDetails}>{listing.description}</Text>
             </View>
-          </View>
+          </Animated.ScrollView>
         </View>
+        <Animated.View style={styles.footer} entering={SlideInDown.delay(200)}>
+          <TouchableOpacity
+            onPress={() => {}}
+            style={[styles.footerBtn, styles.footerBookBtn]}
+          >
+            <Text style={styles.footerBtnTxt}>Book Now</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {}} style={styles.footerBtn}>
+            <Text style={styles.footerBtnTxt}>Share</Text>
+          </TouchableOpacity>
+        </Animated.View>
       </>
     );
   } else {
@@ -97,7 +197,7 @@ export default ListingDetails;
 
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: Colors.white },
-  contentWrapper: { padding: 20 },
+  contentWrapper: { padding: 20, backgroundColor: Colors.white },
   image: {
     width: width,
     height: IMG_HEIGHT,
@@ -118,5 +218,60 @@ const styles = StyleSheet.create({
     fontSize: 14,
     marginLeft: 5,
     color: Colors.black,
+  },
+  highlightWrapper: {
+    flexDirection: 'row',
+    marginVertical: 20,
+    justifyContent: 'space-between',
+  },
+  highlightIcon: {
+    backgroundColor: '#F4F4F4',
+    padding: 8,
+    borderRadius: 8,
+    marginRight: 5,
+    alignItems: 'center',
+  },
+  highlightTxt: {
+    fontSize: 12,
+    color: '#999',
+  },
+  highlightTxtVal: {
+    fontSize: 14,
+    fontWeight: '600',
+  },
+  listingDetails: {
+    fontSize: 16,
+    color: Colors.black,
+    lineHeight: 25,
+    letterSpacing: 0.5,
+  },
+  footer: {
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 0,
+    padding: 20,
+    paddingBottom: 30,
+    width: width,
+  },
+
+  footerBtn: {
+    flex: 1,
+    backgroundColor: Colors.black,
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+
+  footerBookBtn: {
+    flex: 2,
+    backgroundColor: Colors.primaryColor,
+    marginRight: 20,
+  },
+
+  footerBtnTxt: {
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: '600',
+    textTransform: 'uppercase',
   },
 });
