d955721a38c8539df9b1e1ff4b17d2500ec2682e
hscrown <kwonhs.alice@gmail.com>
2024-05-05 22:35:31 +0900

    지도추가

--- app/(tabs)/category.tsx
+++ app/(tabs)/category.tsx
@@ -1,20 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps'; 
 
 const Page = () => {
   return (
     <View style={styles.container}>
-      <Text>Category</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.5665,   // 서울의 위도
+          longitude: 126.9780, // 서울의 경도
+          latitudeDelta: 0.0922, // 지도의 위도 범위
+          longitudeDelta: 0.0421, // 지도의 경도 범위
+        }}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    map: {
+        width: '100%',  // 지도의 너비
+        height: '100%'  // 지도의 높이
     }
-})
+});
