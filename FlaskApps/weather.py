df904beacac811dfa088198f96dd302c1d78b1d2
hscrown <kwonhs.alice@gmail.com>
2024-05-27 03:57:31 +0900

    feat:update weather.py & add weatherrec.py

--- FlaskApps/weather.py
+++ FlaskApps/weather.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify, request
 import requests
 import pandas as pd
-import numpy as np
 from datetime import datetime
 from lxml import etree
 from haversine import haversine
@@ -9,9 +8,8 @@ from datasets import load_dataset
 
 app = Flask(__name__)
 
-@app.route('/weather',methods=['GET'])
+@app.route('/weather', methods=['GET'])
 def get_weather():
-
     # 오늘 날짜와 시간 불러오기
     what_date = datetime.now().strftime("%Y%m%d")
     what_time = datetime.now().strftime("%H%M")
@@ -25,12 +23,11 @@ def get_weather():
     mylong = request.args.get('longitude')
 
     if not mylat:
-        print("response error:not valid user lat")
+        print("response error: not valid user lat")
         mylat = '37.51490409227562'
     if not mylong:
-        print("response error:not valid user long")
+        print("response error: not valid user long")
         mylong = '126.84135103588255'
-    
 
     # 내 좌표 설정
     my_loc = (float(mylat), float(mylong))
@@ -71,7 +68,7 @@ def get_weather():
         temp = 14
 
     rain_mapping = {
-        '0': "강수없음", 
+        '0': "강수없음",
         '1': "비",
         '2': "비 또는 눈",
         '3': "눈",
@@ -84,8 +81,6 @@ def get_weather():
 
     # 초단기예보데이터
     url2 = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst'
-
-
     response2 = requests.get(url2, params=params)
     root2 = etree.fromstring(response2.content)
 
@@ -104,7 +99,6 @@ def get_weather():
         "ny": item.findtext("ny")
     } for item in items]
 
-
     # 데이터프레임으로 만들기
     df = pd.DataFrame(data)
 
@@ -119,86 +113,17 @@ def get_weather():
     except (KeyError, IndexError):
         sky = "흐림"
 
-    # 날씨 정보에 따른 장소 추천 로직
-    park_name, lib_name, muse_name = None, None, None
-    park_lat, lib_lat, muse_lat = None, None, None
-    park_long, lib_long, muse_long = None, None, None
-    park_adres, lib_adres, muse_adres = None, None, None
-    if (sky in ['맑음', '흐림']) and (rain == '강수없음') and (15 <= float(temp) <= 29):
-        # 공원 정보 불러오기 및 추천 로직
-        park_url = 'http://openAPI.seoul.go.kr:8088/57524f76506d656e3732636a52457a/json/SearchParkInfoService/1/1000/'
-        park_response = requests.get(park_url)
-        park_data = park_response.json()['SearchParkInfoService']['row']
-        park = pd.DataFrame(park_data)
-        park.rename(columns={'P_PARK': "NAME", 'P_ADDR': "ADRES", 'XCNTS': 'LATITUDE', 'YDNTS': "LONGITUDE"}, inplace=True)
-        park['LATITUDE'].replace('', np.nan, inplace=True)
-        park['LONGITUDE'].replace('', np.nan, inplace=True)
-        park = park.dropna()
-        park['LATITUDE'] = park['LATITUDE'].astype(float)
-        park['LONGITUDE'] = park['LONGITUDE'].astype(float)
-        # 가장 가까운 위치 찾기
-        min_distance = float('inf')
-        for index, row in park.iterrows():
-            point = (row['LATITUDE'], row['LONGITUDE'])
-            distance = haversine(my_loc, point)
-            if distance < min_distance:
-                min_distance = distance
-                park_name, park_lat, park_long, park_adres = row['NAME'], row['LATITUDE'], row['LONGITUDE'], row['ADRES']
-    elif rain != '강수없음':
-        # 도서관 정보 불러오기 및 추천 로직
-        lib_url = 'http://openAPI.seoul.go.kr:8088/57524f76506d656e3732636a52457a/json/SeoulLibraryTimeInfo/1/1000/'
-        lib_response = requests.get(lib_url)
-        lib_data = lib_response.json()['SeoulLibraryTimeInfo']['row']
-        lib = pd.DataFrame(lib_data)
-        lib_url2 = 'http://openAPI.seoul.go.kr:8088/57524f76506d656e3732636a52457a/json/SeoulLibraryTimeInfo/1001/2000/'
-        lib_response2 = requests.get(lib_url2)
-        lib_data2 = lib_response2.json()['SeoulLibraryTimeInfo']['row']
-        lib2 = pd.DataFrame(lib_data2)
-        lib = pd.concat([lib,lib2])
-        lib.rename(columns={'LBRRY_NAME': "NAME", 'ADRES': "ADRES", 'XCNTS': 'LATITUDE', 'YDNTS': "LONGITUDE"}, inplace=True)
-        lib['LATITUDE'] = lib['LATITUDE'].astype(float)
-        lib['LONGITUDE'] = lib['LONGITUDE'].astype(float)
-        # 가장 가까운 위치 찾기
-        min_distance = float('inf')
-        for index, row in lib.iterrows():
-            point = (row['LATITUDE'], row['LONGITUDE'])
-            distance = haversine(my_loc, point)
-            if distance < min_distance:
-                min_distance = distance
-                lib_name, lib_lat, lib_long, lib_adres = row['NAME'], row['LATITUDE'], row['LONGITUDE'], row['ADRES']
-    else:
-        # 박물관 정보 불러오기 및 추천 로직
-        muse_url = 'http://openAPI.seoul.go.kr:8088/57524f76506d656e3732636a52457a/json/SeoulMuseumInfo/1/1000/'
-        muse_response = requests.get(muse_url)
-        muse_data = muse_response.json()['SeoulMuseumInfo']['row']
-        muse = pd.DataFrame(muse_data)
-        muse.rename(columns={'MUSEUM_NAME': "NAME", 'ADDR': "ADRES", 'XCNTS': 'LATITUDE', 'YDNTS': "LONGITUDE"}, inplace=True)
-        muse['LATITUDE'] = muse['LATITUDE'].astype(float)
-        muse['LONGITUDE'] = muse['LONGITUDE'].astype(float)
-        # 가장 가까운 위치 찾기
-        min_distance = float('inf')
-        for index, row in muse.iterrows():
-            point = (row['LATITUDE'], row['LONGITUDE'])
-            distance = haversine(my_loc, point)
-            if distance < min_distance:
-                min_distance = distance
-                muse_name, muse_lat, muse_long, muse_adres = row['NAME'], row['LATITUDE'], row['LONGITUDE'], row['ADRES']
-
     # 결과 출력
     result = {
         "sky": sky,
         "rain": rain,
         "temp": temp,
-        "name": park_name or lib_name or muse_name,
-        "latitude": park_lat or lib_lat or muse_lat,
-        "longitude": park_long or lib_long or muse_long,
-        "address": park_adres or lib_adres or muse_adres,
-        "place_type": "공원" if park_name else "도서관" if lib_name else "박물관",
-        "guName" : guName,
-        "dongName": dongName        
+        "guName": guName,
+        "dongName": dongName
     }
     response = jsonify(result)
-    response.headers.add('Content-Type', 'application/json; charset=ASCII') #인코딩 문제 해결
+    response.headers.add('Content-Type', 'application/json; charset=ASCII')  # 인코딩 문제 해결
     return response
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
---
+++ FlaskApps/weatherrec.py
@@ -0,0 +1,99 @@
+# weatherrec.py
+from flask import Flask, jsonify, request
+import requests
+import pandas as pd
+import numpy as np
+from haversine import haversine
+
+app = Flask(__name__)
+
+@app.route('/weatherrec', methods=['GET'])
+def get_place_recommendation():
+    mylat = request.args.get('latitude')
+    mylong = request.args.get('longitude')
+    sky = request.args.get('sky')
+    rain = request.args.get('rain')
+    temp = request.args.get('temp')
+
+    if not mylat or not mylong or not sky or not rain or not temp:
+        return jsonify({"error": "Missing required parameters"}), 400
+
+    # 내 좌표 설정
+    my_loc = (float(mylat), float(mylong))
+
+    park_name, lib_name, muse_name = None, None, None
+    park_lat, lib_lat, muse_lat = None, None, None
+    park_long, lib_long, muse_long = None, None, None
+    park_adres, lib_adres, muse_adres = None, None, None
+
+    if (sky in ['맑음', '흐림']) and (rain == '강수없음') and (15 <= float(temp) <= 29):
+        # 공원 정보 불러오기 및 추천 로직
+        park_url = 'http://openAPI.seoul.go.kr:8088/57524f76506d656e3732636a52457a/json/SearchParkInfoService/1/1000/'
+        park_response = requests.get(park_url)
+        park_data = park_response.json()['SearchParkInfoService']['row']
+        park = pd.DataFrame(park_data)
+        park.rename(columns={'P_PARK': "NAME", 'P_ADDR': "ADRES", 'XCNTS': 'LATITUDE', 'YDNTS': "LONGITUDE"}, inplace=True)
+        park['LATITUDE'].replace('', np.nan, inplace=True)
+        park['LONGITUDE'].replace('', np.nan, inplace=True)
+        park = park.dropna()
+        park['LATITUDE'] = park['LATITUDE'].astype(float)
+        park['LONGITUDE'] = park['LONGITUDE'].astype(float)
+        # 가장 가까운 위치 찾기
+        min_distance = float('inf')
+        for index, row in park.iterrows():
+            point = (row['LATITUDE'], row['LONGITUDE'])
+            distance = haversine(my_loc, point)
+            if distance < min_distance:
+                min_distance = distance
+                park_name, park_lat, park_long, park_adres = row['NAME'], row['LATITUDE'], row['LONGITUDE'], row['ADRES']
+    elif rain != '강수없음':
+        # 도서관 정보 불러오기 및 추천 로직
+        lib_url = 'http://openAPI.seoul.go.kr:8088/57524f76506d656e3732636a52457a/json/SeoulLibraryTimeInfo/1/1000/'
+        lib_response = requests.get(lib_url)
+        lib_data = lib_response.json()['SeoulLibraryTimeInfo']['row']
+        lib = pd.DataFrame(lib_data)
+        lib_url2 = 'http://openAPI.seoul.go.kr:8088/57524f76506d656e3732636a52457a/json/SeoulLibraryTimeInfo/1001/2000/'
+        lib_response2 = requests.get(lib_url2)
+        lib_data2 = lib_response2.json()['SeoulLibraryTimeInfo']['row']
+        lib2 = pd.DataFrame(lib_data2)
+        lib = pd.concat([lib,lib2])
+        lib.rename(columns={'LBRRY_NAME': "NAME", 'ADRES': "ADRES", 'XCNTS': 'LATITUDE', 'YDNTS': "LONGITUDE"}, inplace=True)
+        lib['LATITUDE'] = lib['LATITUDE'].astype(float)
+        lib['LONGITUDE'] = lib['LONGITUDE'].astype(float)
+        # 가장 가까운 위치 찾기
+        min_distance = float('inf')
+        for index, row in lib.iterrows():
+            point = (row['LATITUDE'], row['LONGITUDE'])
+            distance = haversine(my_loc, point)
+            if distance < min_distance:
+                min_distance = distance
+                lib_name, lib_lat, lib_long, lib_adres = row['NAME'], row['LATITUDE'], row['LONGITUDE'], row['ADRES']
+    else:
+        # 박물관 정보 불러오기 및 추천 로직
+        muse_url = 'http://openAPI.seoul.go.kr:8088/57524f76506d656e3732636a52457a/json/SeoulMuseumInfo/1/1000/'
+        muse_response = requests.get(muse_url)
+        muse_data = muse_response.json()['SeoulMuseumInfo']['row']
+        muse = pd.DataFrame(muse_data)
+        muse.rename(columns={'MUSEUM_NAME': "NAME", 'ADDR': "ADRES", 'XCNTS': 'LATITUDE', 'YDNTS': "LONGITUDE"}, inplace=True)
+        muse['LATITUDE'] = muse['LATITUDE'].astype(float)
+        muse['LONGITUDE'] = muse['LONGITUDE'].astype(float)
+        # 가장 가까운 위치 찾기
+        min_distance = float('inf')
+        for index, row in muse.iterrows():
+            point = (row['LATITUDE'], row['LONGITUDE'])
+            distance = haversine(my_loc, point)
+            if distance < min_distance:
+                min_distance = distance
+                muse_name, muse_lat, muse_long, muse_adres = row['NAME'], row['LATITUDE'], row['LONGITUDE'], row['ADRES']
+
+    result = {
+        "name": park_name or lib_name or muse_name,
+        "latitude": park_lat or lib_lat or muse_lat,
+        "longitude": park_long or lib_long or muse_long,
+        "address": park_adres or lib_adres or muse_adres,
+        "place_type": "공원" if park_name else "도서관" if lib_name else "박물관"
+    }
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
